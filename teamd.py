f028acdfa1a5b24dfabd21639005bb071e3ab9b7
dominickdelpico <57916511+dominickdelpico@users.noreply.github.com>
2020-01-30 14:18:10 -0500

    Update teamd.py

--- teamd.py
+++ teamd.py
@@ -2,3 +2,8 @@
 roomArray = []
 for i in range(999):
     roomArray.append(False)
+
+roomArray[102] = "You are standing in the corner of the dark prison cell. \nTo the east is a bed. To the south is a toilet."
+roomArray[103] = "You are now infront of your bed. The matress is made out of cardboard, and the pillow is a stuffed aligator. \nTo the west is another part of your cell. To the south is the cell door."
+roomArray[202] = "Here is your toilet, or rather your bucket. You have fond memories of using this against the gaurd. \nTo the north is the cell corner. To the east is the cell door."
+roomArray[203] = "The cell door is open, so it must be break time. You can continue east down the hallway towards the mess. \nTo the north is your bed, and to the west is the toilet."
